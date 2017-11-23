1e011ba020d01e6b345d08d75850a3aee2bc9858
jeanpan <jean.pan@kaiostech.com>
2017-11-23 18:11:26 +0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  env: {
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  plugins: [
+    'prettier',
+  ],
+  rules: {
+    'no-param-reassign': 'off',     // Needed for swapping elements within an array.
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',  // Indicate private methods.
+  },
+};
---
+++ README.md
@@ -0,0 +1,9 @@
+## js-project-starter
+
+### Setup
+
+```sh
+$ git clone https://github.com/clapperboard/js-project-starter {your_new_project}
+$ cd {your_new_project}
+$ yarn # or npm install
+```
